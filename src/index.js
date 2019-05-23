366c06c5470fada48f14e1b20524190d2a7bc33f
ProgRockPaul <twentytwentyk@live.com>
2019-05-22 20:42:24 -0400

    first

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import "dotenv/config";
+import cors from "cors";
+import express from "express";
+import bodyParser from "body-parser";
+const app = express();
+const port = process.env.PORT;
+import models from "./models";
+import routes from "./routes";
+
+app.use(cors);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  req.context = {
+    models,
+    me: models.users[1]
+  };
+  next();
+});
+app.use("/session", routes.session);
+app.use("/users", routes.user);
+app.use("/messages", routes.message);
+
+app.get("/", (req, res) => {
+  return res.send("Hey welcome");
+});
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
---
+++ src/models/index.js
@@ -0,0 +1,28 @@
+let users = {
+  1: {
+    id: "1",
+    username: "Paul Walsh"
+  },
+  2: {
+    id: "2",
+    username: "Mega Mouse"
+  }
+};
+
+let messages = {
+  1: {
+    id: "1",
+    text: "API Response",
+    userId: "1"
+  },
+  2: {
+    id: "2",
+    text: "SUUUUP",
+    userId: "2"
+  }
+};
+
+export default {
+  users,
+  messages
+};
